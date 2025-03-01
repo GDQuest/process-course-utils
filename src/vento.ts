9acd8bde65baee9d4e478a539e0ea25f8c3477d8
xananax <me@xananax.com>
2025-03-01 19:18:07 +0100

    chore: better vento API

--- src/vento.ts
+++ src/vento.ts
@@ -1,11 +1,20 @@
-import vento, { type Options } from "jsr:@vento/vento";
+import vento, { type Options as VentoOptions } from "jsr:@vento/vento";
+import { type Template as VentoTemplate, Environment as VentoEnvironment } from "jsr:@vento/vento/src/environment.ts";
 import { existsSync } from "jsr:@std/fs@^1.0.5/exists";
 import { writeOutputFile } from "./writeOutputFile.ts";
 
 
-interface AugmentedVentoOptions extends Omit<Options, 'includes'> {
+export { VentoTemplate}
+
+interface AugmentedVentoOptions extends Omit<VentoOptions, 'includes'> {
   includes: string,
   siteTitle: string
+}
+
+export interface TemplateWriter{
+  template: VentoTemplate,
+  render: (data: Record<string, unknown>) => Promise<string>,
+  renderAndWrite: (destDir: string, data: Record<string, unknown>, filenameWithoutExtension?: string) => Promise<void>
 }
 
 export const makeVento = ({siteTitle, ...options}: AugmentedVentoOptions) => {
@@ -14,38 +23,45 @@ export const makeVento = ({siteTitle, ...options}: AugmentedVentoOptions) => {
   }
   const env = vento(options)
 
-  const renderAndWriteFile =
-    (template: string) =>
-    async (
-      directory: string,
-      fileNameWithoutExtension: string,
-      data: Record<string, unknown>
-    ) => {
-      writeOutputFile(
-        directory,
-        fileNameWithoutExtension + ".html",
-        false,
-        await render(template, data)
-      );
-    };
-
-  const render = async (template: string, data: Record<string, unknown>, from?: string) => {
+  const augmentTemplateData = (data: Record<string, unknown>) => {
+    if ('pageTitle' in data) {
+      return data;
+    }
     const { title } = data;
     const pageTitle =
       typeof title === "string" && title.length > 0
         ? `${title} | ${siteTitle}`
         : siteTitle;
-    const result = await env.run(template, {
-      pageTitle,
-      ...data,
-    }, from);
+    return { pageTitle, ...data };
+  }
+
+
+  const makeTemplateWriter =
+    async (templateFileName: string, initialData?: Record<string, unknown>, from?: string): Promise<TemplateWriter> => {
+          const template = await env.load(templateFileName, from);
+          const render = async (data: Record<string, unknown>) => {
+            const _data = initialData ? { ...initialData, ...data } : data;
+            const result = await template(augmentTemplateData(_data));
+            return result.content;
+          }
+          const renderAndWrite = async (destDir: string, data: Record<string, unknown>, filenameWithoutExtension: string = "index") => {
+            const result = await render(data);
+            await writeOutputFile(destDir, filenameWithoutExtension + '.html', false, result);
+          }
+          return { template, render, renderAndWrite };
+    }
+
+  const render = async (templateFilePath: string, data: Record<string, unknown>, from?: string) => {
+    const result = await env.run(templateFilePath, augmentTemplateData(data), from);
     return result.content
   };
 
-  const renderString = async (template: string, data: Record<string, unknown>) => {
-    const result = await env.runString(template, data);
+  const renderString = async (templateFilePath: string, data: Record<string, unknown>) => {
+    const result = await env.runString(templateFilePath, data);
     return result.content;
   }
 
-  return { renderAndWriteFile, render, renderString };
+  const prepareTemplate = async (templateFilePath: string, from?: string) => await env.load(templateFilePath, from);
+
+  return { makeTemplateWriter, render, renderString, prepareTemplate };
 }
